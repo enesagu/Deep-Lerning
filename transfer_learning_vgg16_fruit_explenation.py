ad74c19d1e9e56afc9ae46d13c7cfb171e5daa4e
Enes Agu <97469455+enesagu@users.noreply.github.com>
2024-03-22 18:02:25 +0300

    Create transfer_learning_vgg16_explenation file
    I just import some package before the starting write code. HA  HA  HA HAVE A NICE DAYYY :)

---
+++ transfer_learning_vgg16_fruit_explenation.py
@@ -0,0 +1,6 @@
+from keras import ImageDataGenerator, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt 
+from glob import glob
+
